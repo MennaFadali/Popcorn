bbc4d9665eecee579b22923be262de66d3dcbcc8
YasmineMaheeb <yasminemaheeb@gmail.com>
2019-12-06 13:35:31 +0200

    viewing created movie nights

--- api/view.js
+++ api/view.js
@@ -8,18 +8,33 @@ const bodyParser = require("body-parser");
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
+router.get("/", async (req, res) => {
+    var ht = "<div><h1>Created Movie Nights</h1>";
+    const movienights = await MovieNight.find();
+    for(var i=0;i<movienights.length;++i){
+        const cur = movienights[i];
+        const plot = await functions.getMovieSummary(cur.title);
+        ht += "<h3> Title: "+cur.title+"</h3>";
+        ht += "<h3> Host Name: "+cur.username+"</h3>";
+        ht += "<h3> Host Number: "+cur.phone_number+"</h3>";
+        ht += "<h3> Location: "+cur.location+"</h3>";
+        ht += "<h3> Time: "+cur.time+"</h3>";
+        ht += "<p>"+plot+"</p></br>";
+    }
+    ht+= "</div>";
+    res.send(ht);
+  });
+
+
 // to call: const plot = await functions.getMovieSummary("Tangled");
 const functions = {
   getMovieSummary: async title => {
     try {
-      console.log("enter");
       var mov = await imdb.get(
         { name: title },
         { apiKey: "feb050e5", timeout: 30000 }
       );
-      console.log(mov);
       return mov.plot ? mov.plot : "No info found for this movie";
-      // console.log(mov);
     } catch (err) {
       return "No info found for this movie";
     }
